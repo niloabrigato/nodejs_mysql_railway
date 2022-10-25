7578944794e9c838611cd27e14428fb0d06ef111
nilux <niloabrigato@yahoo.com.ar>
2022-10-25 00:43:55 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express  from "express"
+import { pool } from "./db.js"
+import {
+  PORT
+} from "./config.js"
+
+const app = express()
+
+app.listen(PORT)
+
+app.get('/',  async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM users')
+  res.json(rows)
+})
+
+app.get('/ping', async (req, res) => {
+  const [result] = await pool.query(`SELECT "hello world" as RESULT`)
+  res.json(result[0])
+})
+
+app.get('/create', async (req, res) => {
+  const result = await pool.query('INSERT INTO users (name) VALUES ("Jhon")')
+  res.json(result)
+})
+
+console.log('Servidor en port', PORT)
